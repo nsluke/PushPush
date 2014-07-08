84a72f64b78a9deca9402899b00e0c450e10e684
Ares42 <Luke Solomon>
2014-07-08 17:04:29 -0400

    Initial Commit

---
+++ PushPush/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  PushPush
+//
+//  Created by Luke Solomon on 7/8/14.
+//  Copyright Luke Solomon 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
