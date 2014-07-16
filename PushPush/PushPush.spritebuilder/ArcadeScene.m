08d7110525475ae62e9670645270af4eba6bc3a8
Ares42 <Luke Solomon>
2014-07-16 16:51:11 -0400

    Got the multitouch working. Dividing the screen into 4 quadrants and registering the touches to call the actual pushbutton methods. Added a moveTopLine function, renamed moveLine to moveBottomLine.

--- PushPush/PushPush.spritebuilder/ArcadeScene.m
+++ PushPush/PushPush.spritebuilder/ArcadeScene.m
@@ -19,176 +19,185 @@
     CCNodeColor *BottomRightLine;
     
     CCButton *rightPush;
-    CCButton *_leftPush;
+    CCButton *leftPush;
     
-    //CCParticleSystem *hitMarkL;
+    CCParticleSystem *hitMarkL;
     
     int yVal;
     int pushaT;
     
     FormationHandler *BottomHandler;
     FormationHandler *TopHandler;
-
     
+#define SW [[CCDirector sharedDirector] viewSize].width
+#define SH [[CCDirector sharedDirector] viewSize].height
+
 }
 
-/*
- Five formations will be designed
- 
- --right push--
- right lane gets +2
- middle lane gets +0
- left lane gets -1
- *RIGHT SWIPE*
- /\/\ - \/
- --------------
- 
- --left push--
- right lane gets -1
- middle lane gets +0
- left lane gets +2
- *LEFT SWIPE*
- \/ - //\\
- --------------
- 
- --middle push--
- right lane gets -1
- middle lane gets +3
- left lane gets -1
- *TWO FINGER SWIPE UP or TWO FORWARD SWIPES*
- \/ /\/\/\ \/
- --------------
- 
- --left & right push--
- right lane gets +1.5
- middle lane gets -2
- left lane gets +1.5
- *TWO FINGER SWIPE LEFT AND RIGHT*
- /\  \/\/ /\
- --------------
- 
- --flat--
- right lane gets +.3333
- middle lane gets +.3333
- left lane gets +.3333
- *SWIPE DOWN*
- /\ /\ /\
- --------------
- 
- 
- 
- --------------------
- 6 instance variables. 1 for each line
- line 1 - BtmL
- line 2 - BtmM
- line 3 - BtmR
- line 4 - TopL
- line 5 - TopM
- 
- 
- */
+
 
 
 - (void) didLoadFromCCB {
+    
+    yVal = 5;
+    
     // tell this scene to accept touches
     self.userInteractionEnabled = TRUE;
+    self.multipleTouchEnabled = TRUE;
     
+
+}
+
+// --------------------------------------------------------------------------------------------------------
+#pragma mark Touch Input
+
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+
+    //NSArray *touchArray = [touch allObjects];
+    
+    CGPoint touchLocation = [touch locationInWorld];
+
+    
+    if (touchLocation.x<SW/2 && touchLocation.y<SH/2){
+        //bottom left quadrant
+        CCLOG(@"push detected");
+
+        [self leftPush];
+        
+
+    }
+    
+    
+    else if (touchLocation.x>SW/2 && touchLocation.y<SH/2){
+        //bottom right quadrant
+        [self rightPush];
+        
+        CCLOG(@"push detected");
+
+    }
+    
+    
+    else if (touchLocation.x<SW/2 && touchLocation.y>SH/2){
+        //top left quadrant
+        [self topLeftPush];
+        
+        CCLOG(@"push detected");
+
+    }
+
+   
+    else if (touchLocation.x>SW/2 && touchLocation.y>SH/2){
+         //top right quadrant
+        [self topRightPush];
+        
+        CCLOG(@"push detected");
+
+    }
+    
+}
+
+
+
+
+
+
+
+
+// --------------------------------------------------------------------------------------------------------
+#pragma mark Push Buttons
+- (void) leftPush {
+    
+    //CCLOG(@"leftPush button pressed");
+    
+    [leftPush.parent addChild:hitMarkL];
+    
+//    if (pushaT == 1) {
+//        pushaT = 0;
+        [self moveBottomLine:yVal];
+
+}
+
+- (void) rightPush {
+    
+    //CCLOG(@"rightPush button pressed");
+    
+//    if (pushaT == 0) {
+//        pushaT = 1;
+        [self moveBottomLine:yVal];
+    
+}
+
+-(void) topRightPush {
+    
+    //CCLOG(@"topRightPush button pressed");
+    
+    //    if (pushaT == 0) {
+    //        pushaT = 1;
+    [self moveTopLine:yVal];
+}
+
+-(void) topLeftPush {
+    
+    //CCLOG(@"topLeftPush button pressed");
+    
+    //    if (pushaT == 0) {
+    //        pushaT = 1;
+    [self moveTopLine:yVal];
     
 }
 
 // --------------------------------------------------------------------------------------------------------
-//#pragma mark Push Buttons
+#pragma mark moving the lines
 
--(void) onEnter {
-    //midLine = [CCBReader load:@"Line"];
+- (void) moveBottomLine: (int) dDist {
     
-    yVal = 2;
-    
-    [super onEnter];
-
-    [super setMultipleTouchEnabled:YES];
-    
-    //hitMarkL = (CCParticleSystem *)[CCBReader load:@"hitMarkLeft"];
-
-}
-
-- (void) leftPush {
-    
-    CCLOG(@"leftPush button pressed");
-
-    
-    //[_leftPush.parent addChild:hitMarkL];
-    
-//    if (pushaT == 1) {
-//        pushaT = 0;
-//        
-        [self moveLine:yVal];
-    
-//    } else {
-//        CCLOG(@"Can't push the left button twice!");
-//    }
-
-
-    
-    
-}
-
-- (void) rightPush {
-    //runs a check to make sure that the same button has not been pushed twice
-    
-    CCLOG(@"rightPush button pressed");
-    
-//    if (pushaT == 0) {
-//        pushaT = 1;
-//        
-        [self moveLine:yVal];
-//        
-//    } else {
-//        CCLOG(@"Can't push the right button twice!");
-//    }
-    
-
-    
-}
-
-- (void) moveLine: (int) dDist {
-    
+    //Move the top and bottom lines at the same time.
     BottomMidLine.positionInPoints = ccp(BottomMidLine.positionInPoints.x, BottomMidLine.positionInPoints.y + dDist);
-
+    TopMidLine.positionInPoints = ccp(TopMidLine.positionInPoints.x, TopMidLine.positionInPoints.y + dDist);
+    
     BottomRightLine.positionInPoints = ccp(BottomRightLine.positionInPoints.x, BottomRightLine.positionInPoints.y + dDist);
+    TopRightLine.positionInPoints = ccp(TopRightLine.positionInPoints.x, TopRightLine.positionInPoints.y + dDist);
     
     BottomLeftLine.positionInPoints = ccp(BottomLeftLine.positionInPoints.x, BottomLeftLine.positionInPoints.y + dDist);
+    TopLeftLine.positionInPoints = ccp(TopLeftLine.positionInPoints.x, TopLeftLine.positionInPoints.y + dDist);
 
-    NSLog(@"The line should be moving");
+    //NSLog(@"The bottom line should be moving");
+    
+    //hitMarkL = (CCParticleSystem *)[CCBReader load:@"hitMarker"];
+    //NSLog(@"hitmark");
 }
 
-
-
-
-
-
-
+- (void) moveTopLine: (int) dDist {
+    
+    TopMidLine.positionInPoints = ccp(TopMidLine.positionInPoints.x, TopMidLine.positionInPoints.y - dDist);
+    BottomMidLine.positionInPoints = ccp(BottomMidLine.positionInPoints.x, BottomMidLine.positionInPoints.y - dDist);
+    
+    TopRightLine.positionInPoints = ccp(TopRightLine.positionInPoints.x, TopRightLine.positionInPoints.y - dDist);
+    BottomRightLine.positionInPoints = ccp(BottomRightLine.positionInPoints.x, BottomRightLine.positionInPoints.y - dDist);
+    
+    TopLeftLine.positionInPoints = ccp(TopLeftLine.positionInPoints.x, TopLeftLine.positionInPoints.y - dDist);
+    BottomLeftLine.positionInPoints = ccp(BottomLeftLine.positionInPoints.x, BottomLeftLine.positionInPoints.y - dDist);
+    
+    //NSLog(@"The top line should be moving");
+    
+    //hitMarkL = (CCParticleSystem *)[CCBReader load:@"hitMarker"];
+    //NSLog(@"hitmark");
+}
 
  // --------------------------------------------------------------------------------------------------------
  #pragma mark Swiping
  
  - (void) rightSwipe {
     CCLOG(@"player swiped right");
-     
-     [BottomHandler rightPush];
- 
+    [BottomHandler rightPush];
  }
  
  - (void) leftSwipe {
     CCLOG(@"player swiped left");
- 
- 
  }
  
  - (void) doubleUpSwipe {
      CCLOG(@"player double swiped up");
- 
- //
  }
  
  - (void) doubleLeftRightSwipe {
@@ -203,7 +212,14 @@
  //
  }
  
+// --------------------------------------------------------------------------------------------------------
+#pragma mark Pause button
 
- 
+- (void) pause {
+    CCLOG(@"pause");
+    
+    //[BottomHandler rightPush];
+    
+}
  
 @end
--- PushPush/PushPush.spritebuilder/Source/AppDelegate.m
+++ PushPush/PushPush.spritebuilder/Source/AppDelegate.m
@@ -31,6 +31,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    //Trying to do multi touch stuff....totally lost
+    /*
+    CCGLView *__glView = [CCGLView viewWithFrame: [window_ bounds];
+    [__glView setMultipleTouchEnabled:YES];
+    */
+    
+    
     // Configure Cocos2d with the options set in SpriteBuilder
     NSString* configPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Published-iOS"]; // TODO: add support for Published-Android support
     configPath = [configPath stringByAppendingPathComponent:@"configCocos2d.plist"];
