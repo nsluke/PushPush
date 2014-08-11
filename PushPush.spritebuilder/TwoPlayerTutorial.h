08219b5f61120d876afbfe01ae17466f9d902faf
Ares42 <Luke Solomon>
2014-08-11 13:35:30 -0400

    ummmm

---
+++ PushPush.spritebuilder/TwoPlayerTutorial.h
@@ -0,0 +1,13 @@
+//
+//  TwoPlayerTutorial.h
+//  PushPush
+//
+//  Created by Luke Solomon on 8/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCScene.h"
+
+@interface TwoPlayerTutorial : CCScene
+
+@end
---
+++ PushPush.spritebuilder/TwoPlayerTutorial.m
@@ -0,0 +1,13 @@
+//
+//  TwoPlayerTutorial.m
+//  PushPush
+//
+//  Created by Luke Solomon on 8/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "TwoPlayerTutorial.h"
+
+@implementation TwoPlayerTutorial
+
+@end
