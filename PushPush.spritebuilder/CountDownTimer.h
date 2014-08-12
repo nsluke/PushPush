a405df24c2153efad5054468b4ba961fd35f1adb
Ares42 <Luke Solomon>
2014-08-12 19:48:18 -0400

    working on countdown timer

---
+++ PushPush.spritebuilder/CountDownTimer.h
@@ -0,0 +1,13 @@
+//
+//  CountDownTimer.h
+//  PushPush
+//
+//  Created by Luke Solomon on 8/12/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CountDownTimer : CCLabel
+
+@end
---
+++ PushPush.spritebuilder/CountDownTimer.m
@@ -0,0 +1,13 @@
+//
+//  CountDownTimer.m
+//  PushPush
+//
+//  Created by Luke Solomon on 8/12/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CountDownTimer.h"
+
+@implementation CountDownTimer
+
+@end
