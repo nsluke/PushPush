9bbeed74b6b40edd19e916f9046e9ff5d6930433
Ares42 <Luke Solomon>
2014-08-12 14:58:11 -0400

    finished the tutorial animation

---
+++ PushPush.spritebuilder/TutorialPartTwo.h
@@ -0,0 +1,13 @@
+//
+//  TutorialPartTwo.h
+//  PushPush
+//
+//  Created by Luke Solomon on 8/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCScene.h"
+
+@interface TutorialPartTwo : CCScene
+
+@end
---
+++ PushPush.spritebuilder/TutorialPartTwo.m
@@ -0,0 +1,13 @@
+//
+//  TutorialPartTwo.m
+//  PushPush
+//
+//  Created by Luke Solomon on 8/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "TutorialPartTwo.h"
+
+@implementation TutorialPartTwo
+
+@end
